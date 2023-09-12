30c610afdb040c768210ae8a8c573f1fd2093ac1
MewKay <125735249+MewKay@users.noreply.github.com>
2023-09-12 23:23:43 +0300

    Create playMove function for Computer

--- src/app-logic/Computer.js
+++ src/app-logic/Computer.js
@@ -8,6 +8,19 @@ const Computer = function ComputerFactory() {
     opponent = player;
   };
 
+  const getRandomBetween = function getRandomNumberBetweenMinAndMax(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  const playMove = function makeARandomMoveInsideOpponentGameboard() {
+    const targetX = getRandomBetween(1, 10);
+    const targetY = getRandomBetween(1, 10);
+
+    const targetCoordinates = `${targetX},${targetY}`;
+
+    opponent.gameboard.receiveAttack(targetCoordinates);
+  };
+
   return {
     get gameboard() {
       return gameboard;
