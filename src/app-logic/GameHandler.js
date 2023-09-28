03ac1bc9a823d6939b94dc8516799b264ed02e82
MewKay <125735249+MewKay@users.noreply.github.com>
2023-09-28 18:11:16 +0300

    Populate player1 and player2's gameboards

--- src/app-logic/GameHandler.js
+++ src/app-logic/GameHandler.js
@@ -1,5 +1,6 @@
 import Player from "./Player";
 import Computer from "./Computer";
+import ShipClasses from "./ShipClasses";
 
 const GameHandler = (() => {
   let player1 = undefined;
@@ -8,6 +9,18 @@ const GameHandler = (() => {
   const newGame = function createANewGameWithASetOfPlayers() {
     player1 = Player("Player 1");
     player2 = Computer();
+
+    player1.gameboard.placeShip(ShipClasses.BATTLESHIP, "1,1", false);
+    player1.gameboard.placeShip(ShipClasses.CARRIER, "10,1", true);
+    player1.gameboard.placeShip(ShipClasses.CRUISER, "4,5", true);
+    player1.gameboard.placeShip(ShipClasses.DESTROYER, "8,10", false);
+    player1.gameboard.placeShip(ShipClasses.SUBMARINE, "6,7", false);
+
+    player2.gameboard.placeShip(ShipClasses.BATTLESHIP, "6,5", true);
+    player2.gameboard.placeShip(ShipClasses.CARRIER, "10,5", true);
+    player2.gameboard.placeShip(ShipClasses.CRUISER, "1,1", false);
+    player2.gameboard.placeShip(ShipClasses.DESTROYER, "5,5", true);
+    player2.gameboard.placeShip(ShipClasses.SUBMARINE, "8,4", false);
   };
 
   return {
